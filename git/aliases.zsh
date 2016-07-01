c8fc0b9e14f1455935d898ab0ea0f379a8220919
Alexander Shvets <neochief@shvetsgroup.com>
2016-07-01 10:56:42 +0300

    Recent additions.

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -19,3 +19,4 @@ alias gb='git branch'
 alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
 alias gac='git add -A && git commit -m'
 alias gus='git submodule update --init --recursive'
+alias gpr='git pull --rebase'
