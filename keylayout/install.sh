39b08e0b5f671071ca3505b45561f18e810ec25d
Alexander Shvets <neochief@shvetsgroup.com>
2017-02-24 19:33:05 +0200

    Updated keyboard layout.

--- keylayout/install.sh
+++ keylayout/install.sh
@@ -2,8 +2,8 @@
 
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
-ln -sf "${DIR}"/Russian\ Typography\ +\ Ukrainian.keylayout ~/Library/Keyboard\ Layouts/Russian\ Typography\ +\ Ukrainian.keylayout
+ln -sf "${DIR}"/Russian\ \(Typographic\ +\ Ukrainian\).keylayout ~/Library/Keyboard\ Layouts/Russian\ \(Typographic\ +\ Ukrainian\).keylayout
 
-defaults write com.apple.HIToolbox AppleEnabledInputSources -array '<dict><key>InputSourceKind</key><string>Keyboard Layout</string><key>KeyboardLayout ID</key><integer>252</integer><key>KeyboardLayout Name</key><string>ABC</string></dict>' '<dict><key>InputSourceKind</key><string>Keyboard Layout</string><key>KeyboardLayout ID</key><integer>-20914</integer><key>KeyboardLayout Name</key><string>Russian Typography + Ukrainian</string></dict>'
+defaults write com.apple.HIToolbox AppleEnabledInputSources -array '<dict><key>InputSourceKind</key><string>Keyboard Layout</string><key>KeyboardLayout ID</key><integer>252</integer><key>KeyboardLayout Name</key><string>ABC</string></dict>' '<dict><key>InputSourceKind</key><string>Keyboard Layout</string><key>KeyboardLayout ID</key><integer>-20914</integer><key>KeyboardLayout Name</key><string>Russian (Typographic + Ukrainian)</string></dict>'
 
 defaults write com.apple.symbolichotkeys AppleSymbolicHotKeys -dict-add 60 "{enabled = 1; value = { parameters = (32, 49, 1048576); type = 'standard'; }; }"
